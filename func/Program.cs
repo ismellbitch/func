6fd252f887f828117ce4e6254499e33bad92c6e7
Артём Коваленко <ak7527203@gmail.com>
2022-11-26 17:46:08 +0200

    Add project files.

---
+++ func/Program.cs
@@ -0,0 +1,21 @@
+﻿// вариант 20
+int[,] matrix = new int[5, 5];
+Random r = new Random();
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        matrix[i, j] = r.Next(1, 10);
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+int sum = 0;
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        if (i != j) sum += matrix[i, j];
+    }
+}
+Console.WriteLine(sum);
