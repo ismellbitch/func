e75d7f45e4f720eb9fb1e849b25b40b829d93517
user10 <work20go@mail.ru>
2022-12-19 11:47:56 +0200

    ye)

--- func/Program.cs
+++ func/Program.cs
@@ -1,21 +1,15 @@
-﻿// вариант 20
-int[,] matrix = new int[5, 5];
-Random r = new Random();
-for (int i = 0; i < 5; i++)
+﻿using func;
+
+internal class Program
 {
-    for (int j = 0; j < 5; j++)
+    private static void Main(string[] args)
     {
-        matrix[i, j] = r.Next(1, 10);
-        Console.Write(matrix[i, j] + " ");
-    }
-    Console.WriteLine();
-}
-int sum = 0;
-for (int i = 0; i < 5; i++)
-{
-    for (int j = 0; j < 5; j++)
-    {
-        if (i != j) sum += matrix[i, j];
+        Console.Write("Введите x: ");
+        int x = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[x, x];
+
+        Solution solution = new(x, matrix);
+        Console.WriteLine(solution.GetSolution());
     }
 }
-Console.WriteLine(sum);
---
+++ func/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace func
+{
+    internal class Solution
+    {
+        private int x;
+
+        public int[,] matrix;
+
+        public Solution(int x, int[,] matrix)
+        {
+            this.x = x;
+            this.matrix = matrix;
+        }
+
+        Random r = new Random();
+        public int GetSolution()
+        {
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    matrix[i, j] = r.Next(1, 10);
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            int sum = 0;
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    if (i != j) sum += matrix[i, j];
+                }
+            }
+            return sum;
+        }
+
+    }
+}
